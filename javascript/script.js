cae17b0d876e42d6d7fd203e6e78aba9c341dd63
Angelo Resplandes <angeloodrigues@gmail.com>
2024-06-10 10:00:47 -0300

    fit in menu_moblie

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,11 @@
-window.onload = function () {
-  document.querySelector(".menu_moblie").addEventListener("click", function () {
-    if (document.querySelector(".menu ul").style.display == 'flex') {
-      document.querySelector(".menu ul").style.display = 'none';
-    } else {
-      document.querySelector(".menu ul").style.display = 'flex';
-    }
-  });
-}; 
+document.addEventListener("DOMContentLoaded", function () {
+  window.onload = function () {
+    document.querySelector(".menu_moblie").addEventListener("click", function () {
+      if (document.querySelector(".menu ul").style.display == 'flex') {
+        document.querySelector(".menu ul").style.display = 'none';
+      } else {
+        document.querySelector(".menu ul").style.display = 'flex';
+      }
+    });
+  };
+});
